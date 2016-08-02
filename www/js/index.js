33d220fd8b3d65e7c13b78ea32491916daba6c96
Aleksejs Kovalovs <aleks4444@inbox.lv>
2016-08-02 16:06:58 +0300

    Added artists

--- www/js/index.js
+++ www/js/index.js
@@ -17,11 +17,20 @@ var app = {
 
 
 function init(){
-	$("#mm").html("LOOOOOL");
 	var l1 = mkln([{cls:"pause pa90",nm:"."},{cls:"pause pa60",nm:"."},{cls:"artist ar60",nm:"DARUDE"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"LOST FREQUENCIES"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"KNIFE PARTY"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"DVBBS"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"STEVE AOKI"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"AVICII"}
 ]);
+	var l2 = mkln([{cls:"pause pa90",nm:"."},{cls:"pause pa90",nm:"."},{cls:"artist ar60",nm:"SIGMA"},{cls:"pause pa30",nm:"."},{cls:"artist ar45",nm:"ANGERFEIST"},{cls:"pause pa45",nm:"."},{cls:"artist ar60",nm:"REDFOO"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"INDECTED MUSHROOM"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"EXAMPLE"}]);
+	var l3 = mkln([{cls:"pause pa90",nm:"."},{cls:"pause pa45",nm:"."},{cls:"artist ar45",nm:"JACK PERRY"},{cls:"artist ar45",nm:"MR.BELT & WEZOL"},{cls:"artist ar45",nm:"WATERMARK"},{cls:"artist ar45",nm:"MATISSE & SADKO"},{cls:"artist ar45",nm:"SAM FELDT"},{cls:"artist ar60",nm:"WILL SPARKS"},{cls:"artist ar60",nm:"DJ BL3ND"},{cls:"artist ar60",nm:"TJR"},{cls:"artist ar60",nm:"UMMET OZCAN"}]);
+	var l4 = mkln([{cls: "pause pa90",nm:"."},{cls: "pause pa90",nm:"."},{cls: "artist ar60",nm:"CC LATVIAN WINNER"},{cls: "artist ar60",nm:"CLUBMUSIC TAKEOVER"},{cls: "artist ar60",nm:"PAUL OJA"},{cls: "artist ar60",nm:"BAD J"},{cls: "artist ar60",nm:"HEAVYWEIGHT"},{cls: "artist ar60",nm:"KERT KLAUS"},{cls: "artist ar60",nm:"HRISTJAN HURMO"}]);
+	$("#s1").html('<div class="ss">WKND stage</div>'+l1);
+	$("#s2").html('<div class="ss">WKND Live stage</div>'+l2);
+	$("#s3").html('<div class="ss">Future stage</div>'+l3);
+	$("#s4").html('<div class="ss">Digital stage</div>'+l4);
 
-	$("#s2").html('<div class="ss">WKND stage</div>'+l1);
+	var l12 = mkln([{cls:"pause pa15",nm:"."},{cls:"artist ar60",nm:"TEMPER2"},{cls:"pause pa15",nm:"."},{cls:"artist ar60",nm:"SYN COLE"},{cls:"pause pa15",nm:"."},{cls:"artist ar60",nm:"AXWELL & INGRESSO"},{cls:"pause pa15",nm:"."},{cls:"artist ar60",nm:"OTTO KNOWS"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"SHOWTEK"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"ROBIN SHULZ"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"MARTIN GARRIX"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"TIESTO"}]);
+	var l22 = mkln([{cls:"pause pa60",nm:"."},{cls:"artist ar60",nm:"ELINA BORN"},{cls:"pause pa30",nm:"."},{cls:"artist ar60",nm:"I WEAR EXPERIMENT"},{cls:"pause pa45",nm:"."},{cls:"artist ar60",nm:"PER"},{cls:"pause pa45",nm:"."},{cls:"artist ar60",nm:"ALAN WALKER"},{cls:"pause pa45",nm:"."},{cls:"artist ar60",nm:"BRAINSTORM"}]);
+	$("#s12").html('<div class="ss">WKND stage</div>'+l12);
+	$("#s22").html('<div class="ss">WKND stage</div>'+l22);
 }
 
 
